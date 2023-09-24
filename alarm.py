e6fc6c6b721b9b7ac4a59b91e0619e65fad4f08b
AkinwoleAdam <akinwoleadam@gmail.com>
2023-09-24 17:56:29 +0100

    Practiced more projects

--- alarm.py
+++
@@ -1,7 +0,0 @@
-'''
-Basic Alarm Clock:
-
-Description: Develop a basic alarm clock program that asks the user to set an alarm time. 
-When the alarm time is reached, the program notifies the user.
-Purpose: This project covers user input, time handling, and conditional statements.
-'''
